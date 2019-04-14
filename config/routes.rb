4c10cc46b1079edf6b60b848d581bbac2ebb28ea
Keinick <setoguchitoshi@setoguchitoshinoMacBook-puro.local>
2019-04-14 12:32:16 +0900

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   end
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-    collection do
-      get 'search'
-    end
   end
 end
